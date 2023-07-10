e6000bb95a523c84ae4d8b8648c235b60825fb53
Leonan Müller <leomullerluiz@gmail.com>
2023-07-10 19:14:10 -0300

    Associação

---
+++ aula14.php
@@ -0,0 +1,39 @@
+<?php
+    // Associação
+
+    class Pedido{
+
+        public $num;
+        public $cliente;
+
+
+
+    }
+
+    class Cliente{
+
+        public $nome;
+        public $endereco;
+
+    }
+
+    $cliente = new Cliente();
+    $cliente->nome = "Leo Müller";
+    $cliente->endereco = "Rua dos Alfeneiros, nº 4, Little Whinging";
+
+    $pedido = new Pedido();
+    $pedido->numero = "123";
+    $pedido->cliente = $cliente;
+
+    $dados = array(
+        'numero' => $pedido->numero,
+        'nome_cliente' => $pedido->cliente->nome,
+        'endereco_cliente' => $pedido->cliente->endereco
+    );
+
+    echo "<pre>";
+    var_dump($dados);
+    echo "</pre>";
+
+
+?>
