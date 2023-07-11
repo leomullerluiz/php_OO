3ad7b6caf1c1a709fca622bad7f4cd8056c849b2
Leonan Müller <leomullerluiz@gmail.com>
2023-07-11 19:41:40 -0300

    updating folder names

--- crud/index.php
+++ crud/index.php
@@ -4,4 +4,11 @@
 
     require_once 'vendor/autoload.php';
 
+    $produto = new \App\Model\Produto();
+    $produto->setNome('Trident Max');
+    $produto->setDescricao('5 Unidades');
+
+    var_dump($produto);
+
+
 ?>
